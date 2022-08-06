dc2d85e9c767e298e2f5b16481b8782e02de0072
rusikstayhome <alenrus32@gmail.com>
2022-08-06 18:47:40 +0300

    added components: employees-add-form, employees-list, employees-list-item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,8 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
@@ -14,6 +16,8 @@ function App() {
                 <AppFilter />
             </div>
 
+            <EmployeesList />
+            <EmployeesAddForm />
         </div>
     );
 }
