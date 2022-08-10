1c61ca9fb1058ac5701c51edf0bd1205dab5c57a
rusikstayhome <alenrus32@gmail.com>
2022-08-10 14:51:07 +0300

    optimized onToggle method

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -45,7 +45,7 @@ class App extends Component {
         })
     }
 
-    onToggleIncrease = (id) => {
+    onToggleProp = (id, prop) => {
         // this.setState(({ data }) => {
         //     const index = data.findIndex(elem => elem.id === id);
 
@@ -61,24 +61,16 @@ class App extends Component {
         this.setState(({ data }) => ({
             data: data.map(item => {
                 if (item.id === id) {
-                    return { ...item, increase: !item.increase }
+                    return {
+                        ...item, [prop]: !item[prop]
+                    }
                 }
                 return item;
             })
         }))
     }
 
-    onToggleRise = (id) => {
-        this.setState(({ data }) => ({
-            data: data.map(item => {
-                if (item.id === id) {
-                    return { ...item, rise: !item.rise }
-                }
-                return item;
-            })
-        }))
 
-    }
 
     render() {
         const employees = this.state.data.length;
@@ -97,8 +89,8 @@ class App extends Component {
                 <EmployeesList
                     data={this.state.data}
                     onDelete={this.deleteItem}
-                    onToggleIncrease={this.onToggleIncrease}
-                    onToggleRise={this.onToggleRise} />
+                    onToggleProp={this.onToggleProp}
+                />
                 <EmployeesAddForm onAdd={this.addItem} />
             </div>
         );
